944fb10243ae08ba4a71e668453a915f0f9d8990
Melling <bluebell1183@gmail.com>
2024-10-13 18:34:56 +0900

    Refactor: Remove unnecessary code and add comments for clarity

--- Assets/Scripts/ChapterButton.cs
+++ Assets/Scripts/ChapterButton.cs
@@ -1,12 +1,16 @@
 using UnityEngine;
 
+/// <summary>
+/// 챕터로 이동하는 버튼
+/// </summary>
 public class ChapterButton : MonoBehaviour
 {
+    [Tooltip("이동할 챕터의 scene 이름")]
     [SerializeField] string chapterName;
 
     private void OnMouseDown()
     {
-        Debug.Log("In");
+        // 지정된 챕터의 Scene으로 이동
         LobbyManager.Instance.GoToChapter(chapterName);
     }
 }
--- Assets/Scripts/LobbyManager.cs
+++ Assets/Scripts/LobbyManager.cs
@@ -1,13 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+/// <summary>
+/// Lobby를 관리하는 Manager
+/// </summary>
 public class LobbyManager : MonoBehaviour
 {
     private static LobbyManager instance = new LobbyManager();
     public static LobbyManager Instance => instance;
 
+    #region Unity Event
     private void Awake()
     {
         if (instance == null)
@@ -15,6 +17,12 @@ public class LobbyManager : MonoBehaviour
         else
             Destroy(instance);
     }
+    #endregion
+
+    /// <summary>
+    /// 챕터가 구현된 Scene으로 이동하는 메서드
+    /// </summary>
+    /// <param name="chapterName">이동할 챕터의 scene 이름</param>
     public void GoToChapter(string chapterName)
     {
         if (chapterName == null)
