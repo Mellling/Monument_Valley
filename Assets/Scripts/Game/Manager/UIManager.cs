b092ad52b51b2a7f54bacb0208817e3b214139ce
Melling <bluebell1183@gmail.com>
2024-10-21 21:50:13 +0900

    docs: UIManager 주석 추가

--- Assets/Scripts/Game/Manager/UIManager.cs
+++ Assets/Scripts/Game/Manager/UIManager.cs
@@ -25,17 +25,26 @@ public class UIManager : MonoBehaviour
     }
     #endregion
 
+    #region Open/Colse
+    /// <summary>
+    /// UI 켜는 메서드
+    /// </summary>
+    /// <param name="ui">닫을 UI 오브젝트</param>
     public void UIOpen(GameObject ui)
     {
-        UIHistoryStack.Push(ui);
-        ui.SetActive(true);
+        UIHistoryStack.Push(ui);    // 활성화 할 UI 스택에 Push
+        ui.SetActive(true); // UI 오브젝트 활성화
     }
 
+    /// <summary>
+    /// UI 닫는 메서드
+    /// </summary>
     public void UIClose()
     {
-        if (UIHistoryStack.Count == 0)
+        if (UIHistoryStack.Count == 0)  // 스택 안에 아무 오브젝트도 없을 시
             return;
 
-        UIHistoryStack.Pop().SetActive(false);
+        UIHistoryStack.Pop().SetActive(false);  // 스택에 있는 오브젝트 꺼내 비활성화
     }
+    #endregion
 }
