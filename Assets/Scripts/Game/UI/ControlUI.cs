5e6cb74ce01d6546db190b24ee400c7a4e04c251
Melling <bluebell1183@gmail.com>
2024-10-21 21:39:30 +0900

    feat: 임시로 구현한 코드 지우고 UIManager 이용한 UI Control 구현

--- Assets/Scripts/Game/UI/ControlUI.cs
+++ Assets/Scripts/Game/UI/ControlUI.cs
@@ -1,23 +1,39 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 /// <summary>
 /// 연결된 UI 켜거나 끄는 기능 구현
 /// </summary>
 public class ControlUI : MonoBehaviour
 {
-    [SerializeField] List<GameObject> connectedUI;
+    [SerializeField] GameObject connectedUI;
+    [SerializeField] Button button;
+    [SerializeField] bool isOpenButton;
+
+    private void Start()
+    {
+        if (isOpenButton)
+        {
+            if (UIManager.Instance.UIHistoryStack.Count == 0)
+                UIManager.Instance.UIOpen(gameObject);
+        }
+
+        button.onClick.AddListener(() =>
+        {
+            UIManager.Instance.UIClose();
+            UIManager.Instance.UIOpen(connectedUI);
+        });
+    }
 
     /// <summary>
     /// UI 켜거나 끄는 메서드
     /// </summary>
     public void ControlConnectedUI()
     {
-        foreach (GameObject ui in connectedUI)
+        /*foreach (GameObject ui in connectedUI)
         {
             ui.SetActive(!ui.activeSelf);
-        }
+        }*/
     }
 
     public void ControlPlayerActive(bool active)
